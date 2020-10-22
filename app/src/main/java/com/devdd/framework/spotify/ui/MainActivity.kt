65adebbc8c20b66c375f5ca845312174ac2f09dd
Deepak Dawade <deepakdawade.dd@gmail.com>
2020-10-22 23:55:39 +0530

    data binding in MainActivity

--- app/src/main/java/com/devdd/framework/spotify/ui/MainActivity.kt
+++ app/src/main/java/com/devdd/framework/spotify/ui/MainActivity.kt
@@ -1,24 +1,25 @@
 package com.devdd.framework.spotify.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.media.session.PlaybackStateCompat
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import androidx.navigation.fragment.findNavController
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.RequestManager
 import com.devdd.framework.spotify.R
 import com.devdd.framework.spotify.adapters.SwipeSongAdapter
 import com.devdd.framework.spotify.data.entities.Song
+import com.devdd.framework.spotify.databinding.ActivityMainBinding
 import com.devdd.framework.spotify.exoplayer.isPlaying
 import com.devdd.framework.spotify.exoplayer.toSong
-import com.devdd.framework.spotify.other.Status
 import com.devdd.framework.spotify.other.Status.*
 import com.devdd.framework.spotify.ui.viewmodels.MainViewModel
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -34,19 +35,24 @@ class MainActivity : AppCompatActivity() {
 
     private var curPlayingSong: Song? = null
 
+    private lateinit var navController: NavController
+
     private var playbackState: PlaybackStateCompat? = null
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = requireNotNull(_binding)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         subscribeToObservers()
+        navController = findNavController(R.id.navHostFragment)
+        binding.vpSong.adapter = swipeSongAdapter
 
-        vpSong.adapter = swipeSongAdapter
-
-        vpSong.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+        binding.vpSong.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                if(playbackState?.isPlaying == true) {
+                if (playbackState?.isPlaying == true) {
                     mainViewModel.playOrToggleSong(swipeSongAdapter.songs[position])
                 } else {
                     curPlayingSong = swipeSongAdapter.songs[position]
@@ -54,20 +60,20 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        ivPlayPause.setOnClickListener {
+        binding.ivPlayPause.setOnClickListener {
             curPlayingSong?.let {
                 mainViewModel.playOrToggleSong(it, true)
             }
         }
 
         swipeSongAdapter.setItemClickListener {
-            navHostFragment.findNavController().navigate(
+            navController.navigate(
                 R.id.globalActionToSongFragment
             )
         }
 
-        navHostFragment.findNavController().addOnDestinationChangedListener { _, destination, _ ->
-            when(destination.id) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
                 R.id.songFragment -> hideBottomBar()
                 R.id.homeFragment -> showBottomBar()
                 else -> showBottomBar()
@@ -76,21 +82,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hideBottomBar() {
-        ivCurSongImage.isVisible = false
-        vpSong.isVisible = false
-        ivPlayPause.isVisible = false
+        binding.ivCurSongImage.isVisible = false
+        binding.vpSong.isVisible = false
+        binding.ivPlayPause.isVisible = false
     }
 
     private fun showBottomBar() {
-        ivCurSongImage.isVisible = true
-        vpSong.isVisible = true
-        ivPlayPause.isVisible = true
+        binding.ivCurSongImage.isVisible = true
+        binding.vpSong.isVisible = true
+        binding.ivPlayPause.isVisible = true
     }
 
     private fun switchViewPagerToCurrentSong(song: Song) {
         val newItemIndex = swipeSongAdapter.songs.indexOf(song)
         if (newItemIndex != -1) {
-            vpSong.currentItem = newItemIndex
+            binding.vpSong.currentItem = newItemIndex
             curPlayingSong = song
         }
     }
@@ -104,7 +110,7 @@ class MainActivity : AppCompatActivity() {
                             swipeSongAdapter.songs = songs
                             if (songs.isNotEmpty()) {
                                 glide.load((curPlayingSong ?: songs[0]).imageUrl)
-                                    .into(ivCurSongImage)
+                                    .into(binding.ivCurSongImage)
                             }
                             switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
                         }
@@ -118,12 +124,12 @@ class MainActivity : AppCompatActivity() {
             if (it == null) return@observe
 
             curPlayingSong = it.toSong()
-            glide.load(curPlayingSong?.imageUrl).into(ivCurSongImage)
+            glide.load(curPlayingSong?.imageUrl).into(binding.ivCurSongImage)
             switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
         }
         mainViewModel.playbackState.observe(this) {
             playbackState = it
-            ivPlayPause.setImageResource(
+            binding.ivPlayPause.setImageResource(
                 if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
             )
         }
@@ -131,7 +137,7 @@ class MainActivity : AppCompatActivity() {
             it?.getContentIfNotHandled()?.let { result ->
                 when (result.status) {
                     ERROR -> Snackbar.make(
-                        rootLayout,
+                        binding.rootLayout,
                         result.message ?: "An unknown error occured",
                         Snackbar.LENGTH_LONG
                     ).show()
@@ -143,7 +149,7 @@ class MainActivity : AppCompatActivity() {
             it?.getContentIfNotHandled()?.let { result ->
                 when (result.status) {
                     ERROR -> Snackbar.make(
-                        rootLayout,
+                        binding.rootLayout,
                         result.message ?: "An unknown error occured",
                         Snackbar.LENGTH_LONG
                     ).show()
@@ -152,27 +158,9 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
