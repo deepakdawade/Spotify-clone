e4a31975c047cdbbd9a9b369f168d5b13e2e9ec3
Deepak Dawade <deepakdawade.dd@gmail.com>
2020-10-23 00:00:51 +0530

    data binding in HomeFragment

--- app/src/main/java/com/devdd/framework/spotify/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/devdd/framework/spotify/ui/fragments/HomeFragment.kt
@@ -1,26 +1,37 @@
 package com.devdd.framework.spotify.ui.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.devdd.framework.spotify.R
 import com.devdd.framework.spotify.adapters.SongAdapter
+import com.devdd.framework.spotify.databinding.FragmentHomeBinding
 import com.devdd.framework.spotify.other.Status
 import com.devdd.framework.spotify.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_home.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
 
     private val mainViewModel: MainViewModel by activityViewModels()
 
     @Inject
     lateinit var songAdapter: SongAdapter
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding get() = requireNotNull(_binding)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return _binding?.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,39 +43,28 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         }
     }
 
-    private fun setupRecyclerView() = rvAllSongs.apply {
+    private fun setupRecyclerView() = binding.rvAllSongs.apply {
         adapter = songAdapter
         layoutManager = LinearLayoutManager(requireContext())
     }
 
     private fun subscribeToObservers() {
         mainViewModel.mediaItems.observe(viewLifecycleOwner) { result ->
-            when(result.status) {
+            when (result.status) {
                 Status.SUCCESS -> {
-                    allSongsProgressBar.isVisible = false
+                    binding.allSongsProgressBar.isVisible = false
                     result.data?.let { songs ->
                         songAdapter.songs = songs
                     }
                 }
                 Status.ERROR -> Unit
-                Status.LOADING -> allSongsProgressBar.isVisible = true
+                Status.LOADING -> binding.allSongsProgressBar.isVisible = true
             }
         }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
